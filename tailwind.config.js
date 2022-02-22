9a1f4ece501f939f89b7e8869ff59876e405a710
Maja Sørensen <majabirchsorensen@gmail.com>
2022-02-22 13:52:57 +0100

    vores farver

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      blue: "#0511F2",
+      orange: "#FFA175",
+      green: "#45B773",
+      beige: "#FFF1E1",
+      white: "#FFFFFF",
+      grey: "#505050",
+      black: "#000000",
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
