355a7472ff858e4ce9a3f244ea4c664c73fefc54
Maja Sørensen <majabirchsorensen@gmail.com>
2022-02-23 14:54:51 +0100

    forside ændringer

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,8 @@ module.exports = {
     extend: {
       backgroundImage: {
         bgimg: "url('/img/placeholder.jpg')",
+        forsidebg: "url('/img/img5.webp')",
+        forsideherobg: "url('/img/hero1.webp')",
       },
     },
   },
