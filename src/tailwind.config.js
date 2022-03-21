44f47493a09da76e4606265ff375dbabc4c102bb
Maja Sørensen <majabirchsorensen@gmail.com>
2022-03-21 13:41:49 +0100

    farver + forside

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,25 +1,25 @@
-module.exports = {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
-  theme: {
-    colors: {
-      green: "#93C020",
-      greenHover: "#C0E563",
-      orange: "#FFC466",
+// module.exports = {
+//   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+//   theme: {
+//     colors: {
+//       green: "#93C020",
+//       greenHover: "#C0E563",
+//       orange: "#FFC466",
 
-      white: "#FFFFFF",
-      whiteTransparent: "#hsla(0, 0%, 100%, 0.5)",
-      lightGrey: "#F6F6F6",
-      darkGrey: "#3A3A3A",
-    },
+//       white: "#FFFFFF",
+//       whiteTransparent: "#hsla(0, 0%, 100%, 0.5)",
+//       lightGrey: "#F6F6F6",
+//       darkGrey: "#3A3A3A",
+//     },
 
-    fontSize: {
-      'size-h1': '6.25rem',
-      'size-h2': '5rem',
-      'size-h3': '2.5rem',
-      'size-p': '1.125rem',
-      'size-button': '1.875rem',
-      'size-nav': '1.875rem',
-    }
+//     fontSize: {
+//       'size-h1': '6.25rem',
+//       'size-h2': '5rem',
+//       'size-h3': '2.5rem',
+//       'size-p': '1.125rem',
+//       'size-button': '1.875rem',
+//       'size-nav': '1.875rem',
+//     }
 
     // fontFamily: {
     //   fredoka: "'Fredoka One', cursive",
@@ -28,15 +28,7 @@ module.exports = {
 
     // extend: {
     //   backgroundImage: {
-    //     lokalerherobg: "url('/img/lokaler_hero.png')",
-    //     lokalebannerbg: "url('/img/lokaler_banner.webp')",
-    //     baggrund: "url('/img/baggrund.png')",
-    //     menu: "url('/img/menu.png')",
-    //     forsidebg: "url('/img/img5.webp')",
-    //     kontaktbannerbg: "url('/img/img6.webp')",
-    //     kontaktherobg: "url('/img/hero3.webp')",
-    //     forsideherobg: "url('/img/forside_hero1.webp')",
-    //     forsideherobg: "url('/img/forside_hero.webp')",
+    //     forsidehero: "url('/img/food4.webp')",
     //   },
     //   cursor: {
     //     no: "url(/img/no.svg), pointer",
@@ -46,6 +38,6 @@ module.exports = {
     // width: {
     //   200: "200%",
     // },
-  },
+  // },
   // plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
-};
+// };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,41 +2,33 @@ module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     colors: {
-      blue: "#0511F2",
-      orange: "#FFA175",
-      green: "#45B773",
-      hover: "#5cc185",
-      beige: "#FFF1E1",
+      green: "#93C020",
+      greenHover: "#C0E563",
+      orange: "#FFC466",
+
       white: "#FFFFFF",
-      grey: "#505050",
-      black: "#000000",
+      whiteTransparent: "#hsla(0, 0%, 100%, 0.5)",
+      lightGrey: "#F6F6F6",
+      darkGrey: "#3A3A3A",
     },
 
-    fontFamily: {
-      fredoka: "'Fredoka One', cursive",
-      roboto: "'Roboto Slab', serif",
-    },
+    // fontFamily: {
+    //   fredoka: "'Fredoka One', cursive",
+    //   roboto: "'Roboto Slab', serif",
+    // },
 
     extend: {
       backgroundImage: {
-        lokalerherobg: "url('/img/lokaler_hero.png')",
-        lokalebannerbg: "url('/img/lokaler_banner.webp')",
-        baggrund: "url('/img/food4.webp')",
-        menu: "url('/img/menu.png')",
-        forsidebg: "url('/img/img5.webp')",
-        kontaktbannerbg: "url('/img/img6.webp')",
-        kontaktherobg: "url('/img/hero3.webp')",
-        forsideherobg: "url('/img/forside_hero1.webp')",
-        forsideherobg: "url('/img/forside_hero.webp')",
+        forsidehero: "url('/img/food4.webp')",
       },
-      cursor: {
-        no: "url(/img/no.svg), pointer",
-        soundwaves: "url(/img/soundwaves.svg), pointer",
-      },
+      // cursor: {
+      //   no: "url(/img/no.svg), pointer",
+      //   soundwaves: "url(/img/soundwaves.svg), pointer",
+      // },
     },
-    width: {
-      200: "200%",
-    },
+    // width: {
+    //   200: "200%",
+    // },
   },
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+  // plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
